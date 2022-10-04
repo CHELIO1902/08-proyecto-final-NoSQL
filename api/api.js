ac2fec75736318e969cf692295addcc2c3b7a399
CHELIO1902 <chelio19022012@hotmail.com>
2022-10-04 17:43:54 -0500

    ✨se establece la conexion con la DB

---
+++ api/api.js
@@ -0,0 +1,14 @@
+import express from 'express';
+//import { authRoutes, userRoutes, theaterRoutes } from './routes/index.js'
+
+const api = express()
+
+api.use(express.json())
+
+api.get('/status', (_, res) => {
+  return res.json({
+    msg: 'API Funcionando'
+  })
+})
+
+export default api;
